816370aa101beea3ae6a4197ee2e33298399838c
Sourav Kodali <souravkodali@gmail.com>
2016-04-29 15:02:25 +0530

    change network call to callback insteed of using .then

--- app/ajax.js
+++ app/ajax.js
@@ -3,24 +3,37 @@
 angular.module('Ajaxhelper', [])
   .factory('ajax', ['$http', function($http) {
    return { 
-       getdata: function(method, api, request) {
+       getdata: function(method, api, request, callback) {
       var link = "http://127.0.0.1:3000/api/public/";
       var url = link.concat(api);
       var req;
       if(method === "GET") {
-     //     req = 
-        } else if(method === "POST") {
-           req = {
-           method: 'POST',
-           dataType: 'json',
+          req = {
+           method: 'GET',
+           datatype: 'json',
            url: url,
            headers: {
                'Content-Type': 'application/json'
-           },
-           data: request
+            }
+          }  
+          } else if(method === "POST") {
+            req = {
+            method: 'POST',
+            dataType: 'json',
+            url: url,
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            data: request
           }
-          }
-          return $http(req);  
+      }
+      
+      $http(req).then(function successcallback(response){
+       callback(response.data);
+      },
+      function errorcallback(response){
+       callback(null);
+      });
      }
     };
   }]);
--- app/user.js
+++ app/user.js
@@ -4,16 +4,35 @@ angular.module('register', ['Ajaxhelper'])
   .controller('RegisterController', ['$scope','ajax','$window', function($scope, ajax, $window) {
     $scope.username = "";
     $scope.password = "";
-    var method = 'POST';
-    var api = 'register';
     $scope.register = function() {
+      var method = 'POST';
+      var api = 'register';
       var request = new Object();
       request.username = $scope.username;
       request.password = $scope.password;
       console.log(request);
-      ajax.getdata(method, api, request).then(function(response){
-        $window.alert(response.data.status);
+      ajax.getdata(method, api, request, function(data){
+        $window.alert(data.status);
     });
+    };
+
+    $scope.login = function() {
+      var method = 'POST';
+      var api = 'login';
+      var request = new Object();
+      request.username = $scope.username;
+      request.password = $scope.password;
+      console.log(request);
+     // ajax.getdata(method, api, request).then(function(response){
+     //   $window.alert(response.data.status);
+     ajax.getdata(method, api, request, function(data){
+     if(data == null) {
+        $window.alert("Network Error");
+     } else {
+       $window.alert(data.status);
+     } 
+     });
+   // });
     };
   }]);
 })(window.angular);
